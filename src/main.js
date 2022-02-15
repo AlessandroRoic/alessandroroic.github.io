d0fb2bcf05b20e390ebe45b10e139085d0033250
AlessandroRoic <29387855+AlessandroRoic@users.noreply.github.com>
2022-02-15 21:19:46 +0100

    feat: adds smoothscrolling polyfill

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './styles/main.scss';
 import { store } from '@/store/store';
+import * as smoothscroll from 'smoothscroll-polyfill';
 
+document.title = 'Alessandro Roic';
 const app = createApp(App);
 app.use(store);
 app.mount('#app');
-document.title = 'Alessandro Roic';
+smoothscroll.polyfill();
