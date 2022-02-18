7a9a176df675404ae3ae13adc870b5658e43d236
AlessandroRoic <29387855+AlessandroRoic@users.noreply.github.com>
2022-02-18 17:05:54 +0100

    feat: adds anchor scroll behavior

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './styles/main.scss';
 import { store } from '@/store/store';
-import * as smoothscroll from 'smoothscroll-polyfill';
+import smoothscroll from 'smoothscroll-polyfill';
+import 'smoothscroll-anchor-polyfill';
 
 const app = createApp(App);
 app.use(store);
