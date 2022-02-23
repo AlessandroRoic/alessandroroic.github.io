caf1421d5623629cf9ecdac26da0cd09b637695d
AlessandroRoic <29387855+AlessandroRoic@users.noreply.github.com>
2022-02-23 17:44:15 +0100

    refactor: removed backtop arrow

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,18 @@
   <BaseNavbar />
   <BaseSidenav />
   <HomePage />
-  <BackTopArrow />
-  <BaseFooter />
+  <BaseFooter isFooter="true" />
 </template>
 
 <script>
 import HomePage from '@/views/HomePage';
-import BackTopArrow from '@/components/BackTopArrow';
 import { scrollMixin } from '@/mixins/ScrollMixin';
 import BaseNavbar from '@/components/BaseNavbar';
 import BaseFooter from '@/components/BaseFooter';
 import BaseSidenav from '@/components/BaseSidenav';
 export default {
   name: 'App',
-  components: { BaseSidenav, BaseNavbar, BackTopArrow, HomePage, BaseFooter },
+  components: { BaseSidenav, BaseNavbar, HomePage, BaseFooter },
   mixins: [scrollMixin],
 };
 </script>
