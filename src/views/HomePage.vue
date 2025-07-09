19f53fcf122181786adc46798877a7be6c3c54c9
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2025-07-09 14:58:24 +0100

    info: update info
    Signed-off-by: roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -235,7 +235,7 @@
       <div class="mt-1 mb-1">I'm looking for opportunities, so if you're interested in my resume and want to contact me, please do!</div>
       <div class="text-center">
         <BaseButton>
-          <a class="reset-link" href="mailto:roic.alessandro@gmail.com" target="_blank" aria-label="email">Mail me</a>
+          <a class="reset-link" href="mailto:" target="_blank" aria-label="email">Mail me</a>
         </BaseButton>
       </div>
     </BaseSection>
