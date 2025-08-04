a000c7a4fab6cceab234fecddfac325eb7ca5d65
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2025-08-04 16:49:26 +0100

    fix: correct image imports
    Signed-off-by: roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,26 +1,31 @@
+import siteLogo from '../assets/icons/icons.svg#site-logo';
+import camoNotification from '../assets/images/camo-notifications.webp';
+import eQuality from '../assets/images/e-quality.png'
+import airborneAssault from '../assets/images/airborne-assault.png';
+
 export const projects = [
   {
     name: 'Personal Website',
     description: 'Check this site source code on github for more details',
     site: 'github.com/AlessandroRoic/alessandroroic.github.io',
-    logo: './src/assets/icons/icons.svg#site-logo',
+    logo: siteLogo,
   },
   {
     name: 'CamoNotifications',
     description: 'React notifications library that disguises itself in your application 🥷😶‍🌫️',
     site: 'github.com/AlessandroRoic/CamoNotifications',
-    logo: './src/assets/images/camo-notifications.webp',
+    logo: camoNotification,
   },
   {
     name: 'E-Quality',
     description: 'Children video game on gender equality awareness',
     site: 'github.com/AlessandroRoic/E-Quality',
-    logo: './src/assets/images/e-quality.png',
+    logo: eQuality,
   },
   {
     name: 'Airborne Assault',
     description: 'Video game build with libGDX for the university Java course',
     site: 'github.com/AlessandroRoic/AirborneAssault',
-    logo: './src/assets/images/airborne-assault.png',
+    logo: airborneAssault,
   },
 ];
