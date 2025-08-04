304d34ba30d0e197e9e6e8244d7c4db9188b03bd
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2025-08-04 16:37:54 +0100

    fix: removes purge css
    Signed-off-by: roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import purgeCss from 'vite-plugin-purgecss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    purgeCss({
-      content: [`./src/**/*.vue`, `./index.html`],
-      defaultExtractor: content => {
-        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
-        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
-      },
-      safelist: [
-        /-(leave|enter|appear)(|-(to|from|active))$/,
-        /^(?!(|.*?:)cursor-move).+-move$/,
-        /^router-link(|-exact)-active$/,
-        /data-v-.*/,
-      ],
-    }),
   ],
 });
